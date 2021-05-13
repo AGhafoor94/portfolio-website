ee034c9a353120c0911081712a2643dc2d9f1917
Adnan <adnan_ghafoor94@hotmail.com>
2021-05-13 19:00:45 +0100

    Portfolio added with an array

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 
 export const Contact = () => {
+
     const contacts = [
         {
             name: "GitHub",
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,21 +1,38 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+
 export const Portfolio = () => {
-    const [api, setApi] = useState([])
-    useEffect(()=>{
-        
-    const getGitHubRepos = async () =>{
-        const {data} = await axios.get("https://api.github.com/users/AGhafoor94/repos")
-        setApi(data);
-    }
-    getGitHubRepos()
-    },[api])
+    const portfolio = [
+        {
+            name: "BookStore API",
+            description: "Learning about ASP.NET Core MVC",
+            url: "https://github.com/AGhafoor94/BookStore-API"
+        },
+        {
+            name: "Guessing Game in Rust",
+            description: "Learning about Rust by making a guessing game",
+            url: "https://github.com/AGhafoor94/learning-rust-guessing-game"
+        },
+        {
+            name: "Learning Unity",
+            description: "Learning about C# by making a game in Unity",
+            url: "https://github.com/AGhafoor94/learning-unity-part-2"
+        },
+        {
+            name: "Xibit App",
+            description: "App to find a Safari and Aquarium near your area with React, Google API and MongoDB",
+            url: "https://github.com/AGhafoor94/xibit"
+        },
+        {
+            name: "Xibit API",
+            description: "Xibit API",
+            url: "https://github.com/AGhafoor94/xibit-server"
+        }
+    ]
     return (
         <>
             <div className="container">
                 <div className="row">
                     {
-                        api.map((item, key)=>{
+                        portfolio.map((item, key)=>{
                             return (
                                 <div className="col-sm-12 col-md-3 my-2" key={key}>
                                     <div className="card-group h-100">
@@ -25,7 +42,7 @@ export const Portfolio = () => {
                                             {
                                                 item.description === "" ? <p className="card-text">No Description Available</p>:<p className="card-text">{item.description}</p>
                                             }
-                                            <a href={item.html_url} className="card-text">GitHub URL</a>
+                                            <a href={item.url} className="card-text">GitHub URL</a>
                                             </div>
                                         </div>
                                     </div>
