fc0a3462a8ff6c4f09e908761e075c37efa64d20
Adnan <adnan_ghafoor94@hotmail.com>
2021-05-13 19:37:46 +0100

    Sorting out navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,10 +10,10 @@ export const Navigation = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
             <NavLink className="navbar-brand" to="/portfolio-website/">Home</NavLink>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={()=> setMenu(!menu)}>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={menu === true? "true": "false"} aria-label="Toggle navigation" onClick={()=> setMenu(!menu)}>
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className={menu === false? "collapse navbar-collapse": "collapse navbar-collapse show"} id="navbarNav">
             <ul className="navbar-nav">
                 <li className="nav-item">
                 <NavLink className="nav-link active" aria-current="page" to="/portfolio-website/portfolio">Portfolio</NavLink>
