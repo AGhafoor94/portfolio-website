8dc31fa229621a9bd1b1e3aafb4f8fa809f77947
Adnan <adnan_ghafoor94@hotmail.com>
2021-05-13 18:12:53 +0100

    New website

---
+++ src/components/Routes.js
@@ -0,0 +1,29 @@
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import {Navigation} from "../Navigation";
+import { Contact } from "../pages/Contact";
+import { Home } from "../pages/Home";
+import { Portfolio } from "../pages/Portfolio";
+
+export const Routes = () => {
+  return (
+    <div>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/portfolio-website/" exact>
+            <Home />
+          </Route>
+          <Route path="/portfolio-website/portfolio" exact>
+            <Portfolio />
+          </Route>
+          <Route path="/portfolio-website/contact" exact>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
